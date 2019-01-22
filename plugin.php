7d46ce29f3d3dbb96c54830c6ac91b18c17e43d0
Victoria Pavlova <vpavlova@systemorph.com>
2019-01-23 02:43:58 +0300

    Upped version

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SM Blocks
  * Description: SM Blocks is a Gutenberg plugin created via create-guten-block.
- * Version: 1.1.3
+ * Version: 1.1.4
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
